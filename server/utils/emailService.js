7ebf65ef9ce996211c6b392773a7998a51c4a225
DevineniPhanindra <phanindra.devineni869@gmail.com>
2026-07-30 20:43:31 +0530

    Set connection and greeting timeouts in email service
    Added connection and greeting timeouts for email transport.

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -1,13 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-   host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
 });
 
 //transporter for sending emails
