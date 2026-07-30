81a7ee5eb64b6d52ff62e317ff47f867eb30a0a7
DevineniPhanindra <phanindra.devineni869@gmail.com>
2026-07-30 20:05:30 +0530

    member problem fix1

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const { transporter } = require("./config/email");
 
 require("./jobs/membershipReminder");
 
