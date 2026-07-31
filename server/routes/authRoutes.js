a7d9c82062921117a30ef80be3b61ac8ebb74b97
DevineniPhanindra <phanindra.devineni869@gmail.com>
2026-07-31 10:36:57 +0530

    Add change password route to authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { login } = require("../controllers/authController");
+// Change Password
+router.put("/change-password", authMiddleware, changePassword);
 
 router.post("/login", login);
 
