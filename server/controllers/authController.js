4b86e9f2bb410fe6af361478aa5fa2ebe76e3845
DevineniPhanindra <phanindra.devineni869@gmail.com>
2026-07-31 10:35:17 +0530

    Add changePassword function to authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -43,6 +43,47 @@ const login = async (req, res) => {
   }
 };
 
+const changePassword = async (req, res) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+
+    const user = await User.getById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      currentPassword,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Current password is incorrect",
+      });
+    }
+
+    await User.changePassword(req.user.id, newPassword);
+
+    res.status(200).json({
+      success: true,
+      message: "Password changed successfully",
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   login,
+  changePassword,
 };
