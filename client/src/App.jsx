79ed3eb726e50c1c669d7876f67c4606b3b1e55b
DevineniPhanindra <phanindra.devineni869@gmail.com>
2026-08-01 11:09:27 +0530

    Update routing to include Home and Login pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Members from "./pages/Members";
@@ -11,12 +12,22 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
         <Route path="/dashboard" element={<Dashboard />} />
+
         <Route path="/members" element={<Members />} />
+
         <Route path="/members/add" element={<AddMember />} />
+
         <Route path="/members/edit/:id" element={<EditMember />} />
-        <Route path="/change-password" element={<ChangePassword />} />
+
+        <Route
+          path="/change-password"
+          element={<ChangePassword />}
+        />
       </Routes>
     </BrowserRouter>
   );
